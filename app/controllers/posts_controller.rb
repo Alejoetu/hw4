96cf1252085e87f354dc34734776fa50aefbd019
Alejo Etulain <alejo.etulain@kellogg.northwestern.edu>
2023-06-02 05:53:18 +0000

    sa

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,16 @@
 class PostsController < ApplicationController
 
+  def show
+    @post = Post.find_by({ "id" => params["id"] })
+    @place = Place.find_by({ "id" => @post["place_id"] })
+  end
+
   def new
-    @post = Post.new
-    @post.place_id = params["place_id"]
+    if @current_user
+      @posts = Post.where({ "user_id" => @current_user["id"] })
+      @post = Post.new
+      @post.place_id = params["place_id"]
+    end
   end
 
   def create
@@ -11,8 +19,16 @@ class PostsController < ApplicationController
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
+    @post["user_id"] = @current_user["id"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
 
+  def destroy
+    @post = Post.find_by({ "id" => params["id"] })
+    if @post["user_id"] == @current_user["id"]
+      @post.destroy
+    end
+    redirect_to "/places"
+  end
 end
