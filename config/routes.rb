e1522ab0d4827898f8789a92666b69faa57a0aa0
Alejo Etulain <alejo.etulain@kellogg.northwestern.edu>
2023-06-03 23:14:23 +0000

    qd

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get "/", :controller => "places", :action => "index"
+  get "/", :controller => "marketing_pages", :action => "index"
   resources "places"
   resources "posts"
   resources "sessions"
   resources "users"
+  resources "marketing_pages"
 
   # Login/out
   get "/login", :controller => "sessions", :action => "new"
